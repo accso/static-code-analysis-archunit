61ab0fea104416f0a86db8102e5a64ed42113481
kristines <kristine.schaal@accso.de>
2021-11-14 17:02:35 +0100

    logback for archunit

--- architecture-tests/src/test/java/ArchitectureTest.java
+++ architecture-tests/src/test/java/ArchitectureTest.java
@@ -3,9 +3,29 @@ import com.tngtech.archunit.core.importer.ClassFileImporter;
 import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
 import org.junit.jupiter.api.Test;
 
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+
 class ArchitectureTest {
 
     private JavaClasses classes = new ClassFileImporter().importPackages("de.accso.library");
+
+    @Test
+    void testClassesInCommonMustNotUseOtherClassesExceptStandardClasses() {
+         ArchRuleDefinition.classes().that().resideInAPackage("..common..")
+                .should().onlyDependOnClassesThat().resideInAnyPackage("..common..", "java..", "org..")
+                .check(classes);
+    }
+
+    @Test
+    void testEntityClassesHaveToBeAnnotatedAsSuch() {
+        ArchRuleDefinition.classes().that().resideInAPackage("..model..")
+                .and().areNotEnums()
+                .and().areNotInnerClasses()
+                .should().beAnnotatedWith(Entity.class)
+                .orShould().beAnnotatedWith(Embeddable.class)
+                .check(classes);
+    }
 
     @Test
     void testThatLibraryUtilClassesDependOnlyContainJavaStandardClasses() {
