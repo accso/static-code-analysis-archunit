492ab068f62fc892ed1c2ed39400e5973b1126b1
kristines <kristine.schaal@accso.de>
2021-11-26 14:07:32 +0100

    new examples for freeze and ignore

--- architecture-tests/src/test/java/architecturetests/TestsUsingFreezeAndIgnore.java
+++ architecture-tests/src/test/java/architecturetests/TestsUsingFreezeAndIgnore.java
@@ -2,6 +2,9 @@ package architecturetests;
 
 import com.tngtech.archunit.core.domain.JavaClasses;
 import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import com.tngtech.archunit.library.freeze.FreezingArchRule;
 import org.junit.jupiter.api.Test;
 
 public class TestsUsingFreezeAndIgnore {
@@ -10,10 +13,32 @@ public class TestsUsingFreezeAndIgnore {
     private static JavaClasses classesFromLibraryExample = new ClassFileImporter().importPackages(PACKAGE_PREFIX);
 
     /**
-     * Beispiel 7, 8
+     * Beispiel 7 ignore
      */
 
     @Test
-    void test_something() {
+    void testImplementationsMustResideInPackageImplIgnoreAuthorization() {
+        ArchRuleDefinition.classes()
+                .that()
+                .haveSimpleNameEndingWith("Impl")
+                .should()
+                .resideInAPackage("..impl")
+                .check(classesFromLibraryExample);
+    }
+
+    /**
+     * Beispiel 8 freeze
+     */
+    @Test
+    void testImplementationsMustResideInPackageImplWithFreezing(){
+
+        FreezingArchRule.freeze(
+                ArchRuleDefinition.classes()
+                        .that()
+                        .haveSimpleNameEndingWith("Impl")
+                        .should()
+                        .resideInAPackage("..impl")
+        )
+                .check(classesFromLibraryExample);
     }
 }
