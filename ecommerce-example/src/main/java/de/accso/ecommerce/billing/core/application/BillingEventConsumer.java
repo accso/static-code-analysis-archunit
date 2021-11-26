35e151bc2f8af2a83605ecc033530ef04c0b0308
martin.lehmann@accso.de <martin.lehmann@accso.de>
2021-11-26 15:26:09 +0100

    cleanup

--- ecommerce-example/src/main/java/de/accso/ecommerce/billing/core/application/BillingEventConsumer.java
+++ ecommerce-example/src/main/java/de/accso/ecommerce/billing/core/application/BillingEventConsumer.java
@@ -3,9 +3,6 @@ package de.accso.ecommerce.billing.core.application;
 import de.accso.ecommerce.shipping.api.DeliveryRetourEvent;
 import de.accso.ecommerce.shipping.api.DeliverySentOutEvent;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class BillingEventConsumer {
     void consumeDeliverySentOutEvent(DeliverySentOutEvent event) {
         // nope
